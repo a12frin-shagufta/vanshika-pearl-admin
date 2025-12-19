bfcc23e37da5c1f1fafecf6244fbf7a365c960e6
shagufta Afrin <shagufta7572@gmail.com>
2025-12-19 11:52:26 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Testimonials from './pages/Testimonials';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-export const currency = 'PKR';
+export const currency = '$';
 
 const App = () => {
   const [token, setToken] = useState('');
